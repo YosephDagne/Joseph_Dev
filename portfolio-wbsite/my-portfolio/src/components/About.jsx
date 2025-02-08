fcb56f21228e77ac6f49dbf05fed48dd84b22b17
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-02-08 22:40:44 +0300

    complete about

--- portfolio-wbsite/my-portfolio/src/components/About.jsx
+++ portfolio-wbsite/my-portfolio/src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
       <div className="flex gap-20">
         {/* Left */}
         <div>
-          <img src={assets.jo2} alt="My Profile" />
+          <img className="rounded-3xl" src={assets.jo2} alt="My Profile" />
         </div>
 
         {/* Right */}
@@ -41,23 +41,23 @@ const About = () => {
 
           {/* Skills Section */}
           <div className="flex flex-col gap-5">
-            <div className="flex gap-10 items-center transition-all duration-500">
+            <div className="flex gap-10 items-center hover:scale-110 transition-all duration-500">
               <p className="min-w-[150px] text-2xl font-medium">HTML</p>
               <hr className="w-[90%] border-0 h-2 rounded-[50px] bg-gradient-to-r from-orange-700 to-purple-500 " />
             </div>
-            <div className="flex gap-10 items-center transition-all duration-500">
+            <div className="flex gap-10 items-center hover:scale-110 transition-all duration-500">
               <p className="min-w-[150px] text-2xl font-medium">CSS</p>
               <hr className="w-[75%] border-0 h-2 rounded-[50px] bg-gradient-to-r from-orange-700 to-purple-500 " />
             </div>
-            <div className="flex gap-10 items-center transition-all duration-500">
+            <div className="flex gap-10 items-center hover:scale-110 transition-all duration-500">
               <p className="min-w-[150px] text-2xl font-medium">React JS</p>
-              <hr className="w-[80%] border-0 h-2 rounded-[50px] bg-gradient-to-r from-orange-700 to-purple-500 " />
+              <hr className="w-[80%] border-0 h-2 rounded-[50px] bg-gradient-to-r from-orange-700 to-purple-500  " />
             </div>
-            <div className="flex gap-10 items-center transition-all duration-500">
+            <div className="flex gap-10 items-center hover:scale-110 transition-all duration-500">
               <p className="min-w-[150px] text-2xl font-medium">JavaScript</p>
               <hr className="w-[70%] border-0 h-2 rounded-[50px] bg-gradient-to-r from-orange-700 to-purple-500" />
             </div>
-            <div className="flex gap-10 items-center transition-all duration-500">
+            <div className="flex gap-10 items-center hover:scale-110 transition-all duration-500">
               <p className="min-w-[150px] text-2xl font-medium">Node JS</p>
               <hr className="w-[85%] border-0 h-2 rounded-[50px] bg-gradient-to-r from-orange-700 to-purple-500 " />
             </div>
@@ -66,20 +66,26 @@ const About = () => {
       </div>
 
       {/* Experience Stats */}
-      <div className="flex w-[100%] justify-around mb-20">
-        <div className="flex flex-col items-center gap-2 transition-all duration-500">
-          <h1>2+</h1>
-          <p>Years of Experience</p>
+      <div className="flex w-[100%] justify-around mb-20 ">
+        <div className="flex flex-col items-center gap-2 hover:scale-125 transition-all duration-500">
+          <h1 className="text-6xl bg-gradient-to-r from-purple-800 to-orange-500 bg-clip-text text-transparent">
+            2+
+          </h1>
+          <p className="text-2xl font-medium">Years of Experience</p>
         </div>
         <hr />
-        <div className="flex flex-col items-center gap-2 transition-all duration-500">
-          <h1>10+</h1>
-          <p>Projects Completed</p>
+        <div className="flex flex-col items-center gap-2 hover:scale-125 transition-all duration-500">
+          <h1 className="text-6xl bg-gradient-to-r from-purple-800 to-orange-500 bg-clip-text text-transparent">
+            10+
+          </h1>
+          <p className="text-2xl font-medium">Projects Completed</p>
         </div>
         <hr />
-        <div className="flex flex-col items-center gap-2 transition-all duration-500">
-          <h1>2+</h1>
-          <p>Happy Clients</p>
+        <div className="flex flex-col items-center gap-2 hover:scale-125 transition-all duration-500">
+          <h1 className="text-6xl bg-gradient-to-r from-purple-800 to-orange-500 bg-clip-text text-transparent">
+            20+
+          </h1>
+          <p className="text-2xl font-medium">Happy Clients</p>
         </div>
       </div>
     </div>
