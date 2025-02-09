45e445c78d5f46c3c0360a440143e1f37baa55cd
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-02-09 12:25:21 +0300

    complete service section

--- portfolio-wbsite/my-portfolio/src/components/NavBar.jsx
+++ portfolio-wbsite/my-portfolio/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 
 function NavBar() {
   return (
-    <div className="flex items-center justify-between mt-0 mb-5 px-5">
+    <div className="fixed top-0 left-0 w-full flex items-center justify-between mt-0 mb-5 px-5 z-10 shadow-md">
       <img src={assets.jo} alt="Logo" className="w-32 rounded-lg" />
       <ul className="flex items-center list-none gap-10 text-base">
         <li>HOME</li>
@@ -14,7 +14,7 @@ function NavBar() {
       </ul>
       <div
         className="flex items-center justify-center w-48 h-12 rounded-[50px] bg-gradient-to-r from-orange-400 to-purple-600 text-white cursor-pointer 
-        transition-transform duration-300 text-lg text-center hover:from-orange-500 hover:to-purple-700 hover:scale-110  hover:border-white hover:border-2"
+        transition-transform duration-300 text-lg text-center hover:from-orange-500 hover:to-purple-700 hover:scale-110 hover:border-white hover:border-2"
       >
         Connect with Me
       </div>
--- portfolio-wbsite/my-portfolio/src/components/Services.jsx
+++ portfolio-wbsite/my-portfolio/src/components/Services.jsx
@@ -20,15 +20,19 @@ const Services = () => {
         {Services_Data.map((service, index) => (
           <div
             key={index}
-            className="flex flex-col justify-center gap-[20px] p-[60px] rounded-2xl border-2 transition-all duration-500 cursor-pointer"
+            className="flex flex-col justify-center gap-[20px] p-[60px] rounded-2xl border-2 border-[#d4af37] hover:scale-105 transition-all duration-500 cursor-pointer hover:bg-[linear-gradient(120deg,#333,#555)] text-white shadow-lg shadow-[#d4af37]/30 hover:border-[#dc2626]"
           >
             <h3 className="text-3xl font-semibold">{service.s_no}</h3>
             <h2 className="text-4xl font-extrabold bg-gradient-to-r from-purple-700 to-orange-400 text-transparent bg-clip-text">
               {service.s_name}
             </h2>
-            <p>{service.s_desc}</p>
-            <div>
-              <p>Read More</p>
+            <p className="text-2xl text-[#d4d4d4] leading-10 max-w-[300px]">
+              {service.s_desc}
+            </p>
+            <div className="flex gap-5 items-center mt-5">
+              <p className="text-2xl text-[#d4d4d4] leading-10 max-w-[300px]">
+                Read More
+              </p>
               <img src={assets.arrow_icon} alt="Arrow Icon" />
             </div>
           </div>
