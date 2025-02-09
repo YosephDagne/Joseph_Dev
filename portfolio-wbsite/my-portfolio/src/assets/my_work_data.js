9f618fc969a467e951611048ca6750b86cc2a105
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-02-09 15:31:52 +0300

    mywork data and my work

---
+++ portfolio-wbsite/my-portfolio/src/assets/my_work_data.js
@@ -0,0 +1,41 @@
+import project1 from "./project1.jpg";
+import project2 from "./project2.jpg";
+import project3 from "./project3.jpg";
+import project4 from "./project4.jpg";
+import project5 from "./project5.jpg";
+import project6 from "./project6.jpg"; 
+
+const my_work_data = [
+  {
+    w_no: 1,
+    w_name: "Full-Stack E-commerce",
+    w_image: project1,
+  },
+  {
+    w_no: 2, 
+    w_name: "Snake Game",
+    w_image: project2,
+  },
+  {
+    w_no: 3,
+    w_name: "Tic-Tac-Toe Game",
+    w_image: project3,
+  },
+  {
+    w_no: 4,
+    w_name: "Calculator",
+    w_image: project4,
+  },
+  {
+    w_no: 5,
+    w_name: "Apple Clone",
+    w_image: project5,
+  },
+  {
+    w_no: 6,
+    w_name: "Netflix Clone",
+    w_image: project6,
+  },
+];
+
+export default my_work_data;
--- portfolio-wbsite/my-portfolio/src/components/MyWork.jsx
+++ portfolio-wbsite/my-portfolio/src/components/MyWork.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import my_work_data from "../assets/my_work_data";
 
 const MyWork = () => {
   return (
@@ -8,8 +9,12 @@ const MyWork = () => {
         <h1>My Latest Works</h1>
         <img src={assets.theme_pattern} alt="" />
       </div>
-          <div className="">
-              
+      <div className="">
+        {my_work_data.map((work, index) => { 
+          return (
+            <img key={index} src={work.w_image} alt="" />
+          )
+        })}
       </div>
     </div>
   );
