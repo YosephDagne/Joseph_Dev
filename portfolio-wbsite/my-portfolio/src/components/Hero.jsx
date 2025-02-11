704099f4707e7b441e0a81f2e927da387b84e8c2
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-02-11 22:23:03 +0300

    complete the functionality of nav bar

--- portfolio-wbsite/my-portfolio/src/components/Hero.jsx
+++ portfolio-wbsite/my-portfolio/src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
-    <div id="home" className="flex flex-col items-center text-center gap-6 mt-60">
+    <div
+      id="home"
+      className="flex flex-col items-center text-center gap-6 mt-60"
+    >
       {/* Profile Image */}
       <img
         className="w-40 h-40 rounded-full object-cover shadow-lg"
@@ -28,7 +32,13 @@ const Hero = () => {
       {/* Buttons */}
       <div className="flex gap-6 mt-6">
         <button className="px-6 py-3 bg-gradient-to-r from-orange-500 to-purple-700 text-white text-lg rounded-full transition-transform duration-300 hover:scale-110 shadow-md cursor-pointer">
-          Connect with Me
+          <AnchorLink
+            offset={50}
+            href="#contact"
+            onClick={() => setMenu("contact")}
+          >
+            Connect with Me
+          </AnchorLink>
         </button>
         <button className="px-6 py-3 bg-gradient-to-r from-blue-500 to-green-500 text-white text-lg rounded-full transition-transform duration-300 hover:scale-110 shadow-md cursor-pointer">
           My Resume
--- portfolio-wbsite/my-portfolio/src/components/NavBar.jsx
+++ portfolio-wbsite/my-portfolio/src/components/NavBar.jsx
@@ -10,7 +10,11 @@ function NavBar() {
       <img src={assets.jo} alt="Logo" className="w-32 rounded-lg" />
       <ul className="flex items-center list-none gap-10 text-base">
         <li className="flex flex-col gap-2 cursor-pointer">
-          <AnchorLink href="#home" onClick={() => setMenu("home")}>
+          <AnchorLink
+            className="decoration-0 text-white"
+            href="#home"
+            onClick={() => setMenu("home")}
+          >
             HOME
           </AnchorLink>
           {menu === "home" && (
@@ -90,7 +94,13 @@ function NavBar() {
         className="flex items-center justify-center w-48 h-12 rounded-[50px] bg-gradient-to-r from-orange-400 to-purple-600 text-white cursor-pointer 
         transition-transform duration-300 text-lg text-center hover:from-orange-500 hover:to-purple-700 hover:scale-110 hover:border-white hover:border-2"
       >
-        Connect with Me
+        <AnchorLink
+          offset={50}
+          href="#contact"
+          onClick={() => setMenu("contact")}
+        >
+          Connect with Me
+        </AnchorLink>
       </div>
     </div>
   );
