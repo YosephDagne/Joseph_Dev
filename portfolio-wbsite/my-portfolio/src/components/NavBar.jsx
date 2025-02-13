6526a9d83f6332fddcc382bfa6bd1983b1b7cb63
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-02-13 15:19:45 +0300

    nav bar

--- portfolio-wbsite/my-portfolio/src/components/NavBar.jsx
+++ portfolio-wbsite/my-portfolio/src/components/NavBar.jsx
@@ -1,47 +1,41 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { assets } from "../assets/assets";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-
 function NavBar() {
   const [menu, setMenu] = useState("home");
-  const [isOpen, setIsOpen] = useState(false);
+
+  const menuuRef = useRef();
+  const openleMenu = () => {
+    menuuRef.current.style.right = "0";
+  };
+  const closeMenu = () => {
+    menuuRef.current.style.right = "-350px";
+  };
 
   return (
-    <div className="fixed top-0 left-0 w-full flex items-center justify-between p-5 z-10 shadow-md bg-gray-900">
+    <div className="fixed top-0 left-0 w-full flex items-center justify-between mt-0 mb-5 px-5 z-10 shadow-md bg-gray-900 md:mx-[50px] md:my-5">
       <img src={assets.jo} alt="Logo" className="w-32 rounded-lg" />
-
-      {/* Mobile Menu Toggle */}
-      <div
-        className="md:hidden cursor-pointer"
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <img
-          className="w-6 h-6"
-          src={isOpen ? assets.close : assets.open}
-          alt="menu"
-        />
-      </div>
-
-      {/* Navigation Links */}
+      <img
+        onClick={openleMenu}
+        className="w-4 h-auto md:block md:fixed right-[30px]"
+        src={assets.open}
+        alt=""
+      />
       <ul
-        className={`md:flex items-center list-none gap-10 text-base absolute md:static bg-gray-900 md:bg-transparent top-16 left-0 w-full md:w-auto p-5 md:p-0 transition-transform duration-300 ${
-          isOpen ? "block" : "hidden"
-        } md:flex`}
+        ref={menuuRef}
+        className="flex items-center list-none gap-10 text-base md:fixed md:flex-col md:items-start md:top-0 md:gap-[30px] md:bg-[#1F0016] md:w-[350px] md:h-[100%] md:z-[2] transition-[right] md:duration-500 duration-500 md:right-[-350px]"
       >
         <img
-          className="w-4 h-auto md:hidden"
+          onClick={closeMenu}
+          className="w-4 h-auto md:block relative md:top-[30px] md:left-[290px] md:w-[30px] hidden"
           src={assets.close}
           alt=""
-          onClick={() => setIsOpen(false)}
         />
-        <li className="flex flex-col gap-2 cursor-pointer">
+        <li className="flex flex-col gap-2 cursor-pointer md:text-[30px] md:pl-[100px] md:flex-row md:gap-5">
           <AnchorLink
             className="decoration-0 text-white"
             href="#home"
-            onClick={() => {
-              setMenu("home");
-              setIsOpen(false);
-            }}
+            onClick={() => setMenu("home")}
           >
             HOME
           </AnchorLink>
@@ -53,14 +47,11 @@ function NavBar() {
             />
           )}
         </li>
-        <li className="flex flex-col gap-2 cursor-pointer">
+        <li className="flex flex-col gap-2 cursor-pointer md:text-[30px] md:pl-[100px] md:flex-row md:gap-5">
           <AnchorLink
             offset={50}
             href="#about"
-            onClick={() => {
-              setMenu("about");
-              setIsOpen(false);
-            }}
+            onClick={() => setMenu("about")}
           >
             ABOUT ME
           </AnchorLink>
@@ -72,14 +63,11 @@ function NavBar() {
             />
           )}
         </li>
-        <li className="flex flex-col gap-2 cursor-pointer">
+        <li className="flex flex-col gap-2 cursor-pointer md:text-[30px] md:pl-[100px] md:flex-row md:gap-5">
           <AnchorLink
             offset={50}
             href="#services"
-            onClick={() => {
-              setMenu("services");
-              setIsOpen(false);
-            }}
+            onClick={() => setMenu("services")}
           >
             SERVICES
           </AnchorLink>
@@ -91,14 +79,11 @@ function NavBar() {
             />
           )}
         </li>
-        <li className="flex flex-col gap-2 cursor-pointer">
+        <li className="flex flex-col gap-2 cursor-pointer md:text-[30px] md:pl-[100px] md:flex-row md:gap-5">
           <AnchorLink
             offset={50}
             href="#work"
-            onClick={() => {
-              setMenu("portfolio");
-              setIsOpen(false);
-            }}
+            onClick={() => setMenu("portfolio")}
           >
             PORTFOLIO
           </AnchorLink>
@@ -110,14 +95,11 @@ function NavBar() {
             />
           )}
         </li>
-        <li className="flex flex-col gap-2 cursor-pointer">
+        <li className="flex flex-col gap-2 cursor-pointer md:text-[30px] md:pl-[100px] md:flex-row md:gap-5">
           <AnchorLink
             offset={50}
             href="#contact"
-            onClick={() => {
-              setMenu("contact");
-              setIsOpen(false);
-            }}
+            onClick={() => setMenu("contact")}
           >
             CONTACT
           </AnchorLink>
@@ -130,9 +112,7 @@ function NavBar() {
           )}
         </li>
       </ul>
-
-      {/* Connect Button */}
-      <div className="hidden md:flex items-center justify-center w-48 h-12 rounded-[50px] bg-gradient-to-r from-orange-400 to-purple-600 text-white cursor-pointer transition-transform duration-300 text-lg text-center hover:from-orange-500 hover:to-purple-700 hover:scale-110 hover:border-white hover:border-2">
+      <div className="flex items-center justify-center w-48 h-12 rounded-[50px] bg-gradient-to-r from-orange-400 to-purple-600 text-white cursor-pointer transition-transform duration-300 text-lg text-center hover:from-orange-500 hover:to-purple-700 hover:scale-110 hover:border-white hover:border-2 md:hidden">
         <AnchorLink
           offset={50}
           href="#contact"
@@ -144,5 +124,4 @@ function NavBar() {
     </div>
   );
 }
-
 export default NavBar;
